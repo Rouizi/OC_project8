6592aefcf2a49b9c65a04229fcd671b876561c05
Rouizi <cinorouizi@hotmail.fr>
2019-11-02 23:24:23 +0100

    edit settings.py

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -161,7 +161,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
     )
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+    #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
