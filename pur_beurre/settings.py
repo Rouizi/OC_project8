6184d2982b80509b683a106e90b68f37c1708ce1
Rouizi <cinorouizi@hotmail.fr>
2019-11-04 22:06:57 +0100

    edit settings

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -151,7 +151,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME", "")
 AWS_QUERYSTRING_AUTH = False 
 AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN", "")
-#MEDIA_URL = os.environ.get("MEDIA_URL", "")
+MEDIA_URL = os.environ.get("MEDIA_URL", "")
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 
